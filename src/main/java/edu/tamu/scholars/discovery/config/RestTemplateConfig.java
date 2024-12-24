e680487092b7d27d2c3a378d0f881e82c3e44178
William Welling <wwelling@tamu.edu>
2024-12-24 14:26:29 -0600

    Update http config and rest template

--- src/main/java/edu/tamu/scholars/discovery/config/RestTemplateConfig.java
+++ src/main/java/edu/tamu/scholars/discovery/config/RestTemplateConfig.java
@@ -7,11 +7,13 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.core5.http.impl.DefaultConnectionReuseStrategy;
 import org.apache.hc.core5.util.TimeValue;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 import edu.tamu.scholars.discovery.config.model.HttpConfig;
@@ -22,16 +24,17 @@ public class RestTemplateConfig {
     @Bean
     RestTemplate restTemplate(RestTemplateBuilder builder, HttpConfig config) {
         PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
-            .setMaxConnTotal(50)
-            .setMaxConnPerRoute(20)
+            .setMaxConnTotal(config.getMaxTotalConnections())
+            .setMaxConnPerRoute(config.getMaxConnectionsPerRoute())
             .setDefaultConnectionConfig(ConnectionConfig.custom()
-                .setTimeToLive(TimeValue.ofMinutes(5))
+                .setTimeToLive(TimeValue.ofMinutes(config.getTimeToLive()))
                 .build())
             .build();
 
         CloseableHttpClient httpClient = HttpClients.custom()
             .setConnectionManager(connectionManager)
-            .evictIdleConnections(TimeValue.ofMinutes(1))
+            .setConnectionReuseStrategy(DefaultConnectionReuseStrategy.INSTANCE)
+            .evictIdleConnections(TimeValue.ofMinutes(config.getEvictIdleConnections()))
             .build();
 
         HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
@@ -40,6 +43,7 @@ public class RestTemplateConfig {
             .requestFactory(() -> requestFactory)
             .connectTimeout(Duration.ofSeconds(config.getConnectTimeout()))
             .readTimeout(Duration.ofSeconds(config.getReadTimeout()))
+            .errorHandler(new DefaultResponseErrorHandler())
             .build();
     }
 
--- src/main/java/edu/tamu/scholars/discovery/config/model/HttpConfig.java
+++ src/main/java/edu/tamu/scholars/discovery/config/model/HttpConfig.java
@@ -14,9 +14,51 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "discovery.http")
 public class HttpConfig {
 
+    private int maxTotalConnections = 50;
+
+    private int maxConnectionsPerRoute = 20;
+
+    /** connection time to live in minutes */
+    private int timeToLive = 5;
+
+    /** minutes before evicting idle connections */
+    private int evictIdleConnections = 2;
+
     private int connectTimeout = 5;
 
     private int readTimeout = 30;
+
+    public int getMaxTotalConnections() {
+        return maxTotalConnections;
+    }
+
+    public void setMaxTotalConnections(int maxTotalConnections) {
+        this.maxTotalConnections = maxTotalConnections;
+    }
+
+    public int getMaxConnectionsPerRoute() {
+        return maxConnectionsPerRoute;
+    }
+
+    public void setMaxConnectionsPerRoute(int maxConnectionsPerRoute) {
+        this.maxConnectionsPerRoute = maxConnectionsPerRoute;
+    }
+
+    public int getTimeToLive() {
+        return timeToLive;
+    }
+
+    public void setTimeToLive(int timeToLive) {
+        this.timeToLive = timeToLive;
+    }
+
+    public int getEvictIdleConnections() {
+        return evictIdleConnections;
+    }
+
+    public void setEvictIdleConnections(int evictIdleConnections) {
+        this.evictIdleConnections = evictIdleConnections;
+    }
 
     public int getConnectTimeout() {
         return connectTimeout;
